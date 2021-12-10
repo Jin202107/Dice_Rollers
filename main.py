d9cc4e9c1d90c855db596522a8cb06ce547d35f5
Jin202107 <95710515+Jin202107@users.noreply.github.com>
2021-12-10 10:21:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,44 +15,59 @@ def listDiceFace(lis : list) -> list:
     '''Return each Dice Face list accordingly to the inputed list.\nEX) [1, 2, 3] >> ["⚀", "⚁", "⚂"]'''
     return [getDiceFace(i) for i in lis]
 
-def printDiceRoll(diceRoll : list) -> None:
+def printDiceRoll(diceRoll : list, addInFront = '', addInBack = '') -> None:
     """Print both Dice Roll list and Dice Face list.\nEX) Result : ⚀, ⚁, ⚂ (It's 1, 2, 3)"""
-    print(f'Result : {", ".join(listDiceFace(diceRoll))} (It\'s {", ".join(map(str, diceRoll))})')
+    print(f'{addInFront}Result : {", ".join(listDiceFace(diceRoll))} (It\'s {", ".join(map(str, diceRoll))}){addInBack}')
+
+def partition(symbol = "-", length = 77, middle = "") -> str:
+    """Return partition that made of inputed length, inputed symbol and middle if it inputed."""
+    length -= len(middle) + 2
+    p = (lambda s, l: s * (l // 2))(symbol, length)
+    return symbol * length if middle == "" else f"{p} {middle} {p}"
+
+scores = []
 
 #110 Meter Hurdles
-print("""Throw all five dice, up to 6 times, until you are satisfied with the result.
-The final score will be the total value of all five dice of the last attempt.""")
+print(partition(middle = "110 Meter Hurdles"), """Throw all five dice, up to 6 times, until you are satisfied with the result.
+The final score will be the total value of all five dice of the last attempt.""", partition(), sep = "\n")
+
 final = 0
 for i in range(6):
     rolls = roll(5)
-    printDiceRoll(rolls)
+    printDiceRoll(rolls, addInFront = "\n")
     final = sum(rolls)
-    user = input("Do you want to stop(please answer with yes or no)? ")
+    if i == 5:
+        print("No more attempt remaining...")
+        break
+    user = input("Do you want to stop(please answer with yes or no)?\n")
     if user.lower() == "yes":
         break
-print("\nGame ended")
-print(f"Final Score: {final}")
+
+scores.append(final)
+print(partition(middle = "Gamed ended"), partition(), "\n", partition(middle = "Pole Vault"), sep = "\n")
 
 #Pole-Vault
-print("\n\n")
-print("""Jumping starts at the height of 10 and is increased by 2 each turn. Players take turn for each height or can decide to skip it.
-You have three attempts for each height. Throw two to eight dice to equal or exceeds the current height without any 1s.
-The final socre will be the maximum height which was mastered.""")
+
+print("""Jumping starts at the height of 10 and is increased by 2 each turn.\nPlayers take turn for each height or can decide to skip it.
+You have three attempts for each height.\nThrow two to eight dice to equal or exceeds the current height without any 1s.
+The final socre will be the maximum height which was mastered.""", partition(), sep = "\n")
+
 height = 10
 nexth = 2
 final = 0
 while True:
-    print(f"Current height is {height}.")
-    user = input('Would you like to have turn or skip it(please answer with "have" or "skip")?')
+    print(partition(), f"Current height is {height}.", sep = "\n")
+    user = input('Would you like to have turn or skip it(please answer with "have" or "skip")?\n')
     if user.lower() == "skip":
         height += nexth
+        print(partition())
         continue
     for i in range(3):
         result = 0
         rolls = []
         diceroll = 0
         while True:
-            user = input("How many dice do you want to roll from two to eight dice?")
+            user = input("\nHow many dice do you want to roll from two to eight dice?\n")
             try:
                 diceroll = int(user)
                 if not diceroll <=  1 or diceroll >= 9:
@@ -68,13 +83,13 @@ while True:
             TF = True
             print("Dice roll has 1 included.")
         if result < height or TF:
-            print("Faild.\nPlease retry!!")
+            print(f"Faild.\n{'Please retry!!' if not i == 2 else ''}\n")
             continue
         print(f"Passed the height {height}")
         final = height
         break
     if not final == height:
+        score.append(final)
         break
     height += nexth
-print("\nGame Ended")
-print(f"Final Score : {final}")
+print(partition("Game Ended"), partition(), "\n", partition(middle = "Scores"), f"110 Meter Hurdles - {scores[0]}\nPole-Vault - {scores[1]}", sep = "\n")
